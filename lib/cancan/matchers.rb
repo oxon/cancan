324aff10756c8c238267b2414f50b122f9950532
Guido Gloor <haslo@haslo.ch>
2018-01-22 12:33:46 +0100

    Deprecation fix for failure_messages

--- lib/cancan/matchers.rb
+++ lib/cancan/matchers.rb
@@ -4,11 +4,35 @@ Kernel.const_get(rspec_module)::Matchers.define :be_able_to do |*args|
     ability.can?(*args)
   end
 
-  failure_message_for_should do |ability|
-    "expected to be able to #{args.map(&:inspect).join(" ")}"
+  # Check that RSpec is < 2.99
+  # see https://github.com/CanCanCommunity/cancancan/blob/86c468639b385e2f4c1175f94afded8726a831fc/lib/cancan/matchers.rb
+  if !respond_to?(:failure_message) && respond_to?(:failure_message_for_should)
+    alias :failure_message :failure_message_for_should
   end
 
-  failure_message_for_should_not do |ability|
-    "expected not to be able to #{args.map(&:inspect).join(" ")}"
+  if !respond_to?(:failure_message_when_negated) && respond_to?(:failure_message_for_should_not)
+    alias :failure_message_when_negated :failure_message_for_should_not
+  end
+
+  failure_message do
+    resource = args[1]
+    if resource.instance_of?(Class)
+      "expected to be able to #{args.map(&:to_s).join(' ')}"
+    else
+      "expected to be able to #{args.map(&:inspect).join(' ')}"
+    end
+  end
+
+  failure_message_when_negated do
+    resource = args[1]
+    if resource.instance_of?(Class)
+      "expected not to be able to #{args.map(&:to_s).join(' ')}"
+    else
+      "expected not to be able to #{args.map(&:inspect).join(' ')}"
+    end
+  end
+
+  description do
+    "be able to #{args.map(&:to_s).join(' ')}"
   end
 end
